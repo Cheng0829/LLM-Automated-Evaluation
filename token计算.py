c1eb76ba43390bc4b4361ceab49972fe7ab25d33
Cheng0829 <376023877@qq.com>
2025-03-17 12:04:08 +0800

    cjk

--- token计算.py
+++ token计算.py
@@ -93,66 +93,73 @@ def llm(provider_name, model_name, system_prompt="", user_prompt="", stream=True
         
 
 dataset_list = []
+
+# ceval分为4类，共52个数据集，共1606道题，总token数82070
+# STEM有20个数据集530道题，Social_Science有10个数据集325道题，Humanity有11个数据集312道题，Other有11个数据集439道题
 task_map = {
+    # STEM数据量: 530, token数: 25308
     "STEM": {
-        "Electrical_Engineer": "注册电气工程师", # 42
-        "Metrology_Engineer": "注册计量师", # 29
-        "College_Programming": "大学编程", # 42
-        "Computer_Architecture": "计算机组成", # 26
-        "Operating_System": "操作系统", # 24
-        "Computer_Network": "计算机网络", # 24
-        "Discrete_Mathematics": "离散数学", # 21
-        "Probability_and_Statistics": "概率统计", # 23
-        "Advanced_Mathematics": "高等数学", # 24
-        "College_Chemistry": "大学化学", # 29
-        "College_Physics": "大学物理", # 24
-        "Veterinary_Medicine": "兽医学", # 28
-        "High_School_Biology": "高中生物", # 24
-        "High_School_Chemistry": "高中化学", # 24
-        "High_School_Physics": "高中物理", # 24
-        "High_School_Mathematics": "高中数学", # 23
-        "Middle_School_Chemistry": "初中化学", # 25
-        "Middle_School_Physics": "初中物理", # 24
-        "Middle_School_Biology": "初中生物", # 26
-        "Middle_School_Mathematics": "初中数学", # 24
+        "Electrical_Engineer": "注册电气工程师", # 数据量: 42, token数: 2059
+        "Metrology_Engineer": "注册计量师", # 数据量: 29, token数: 1064
+        "College_Programming": "大学编程", # 数据量: 42, token数: 1773
+        "Computer_Architecture": "计算机组成", # 数据量: 26, token数: 815
+        "Operating_System": "操作系统", # 数据量: 24, token数: 489
+        "Computer_Network": "计算机网络", # 数据量: 24, token数: 889
+        "Discrete_Mathematics": "离散数学", # 数据量: 21, token数: 626
+        "Probability_and_Statistics": "概率统计", # 数据量: 23, token数: 2196
+        "Advanced_Mathematics": "高等数学", # 数据量: 24, token数: 1429
+        "College_Chemistry": "大学化学", # 数据量: 29, token数: 1074
+        "College_Physics": "大学物理", # 数据量: 24, token数: 1913
+        "Veterinary_Medicine": "兽医学", # 数据量: 28, token数: 1021
+        "High_School_Biology": "高中生物", # 数据量: 24, token数: 1371
+        "High_School_Chemistry": "高中化学", # 数据量: 24, token数: 1271
+        "High_School_Physics": "高中物理", # 数据量: 24, token数: 1262
+        "High_School_Mathematics": "高中数学", # 数据量: 23, token数: 1724
+        "Middle_School_Chemistry": "初中化学", # 数据量: 25, token数: 1031
+        "Middle_School_Physics": "初中物理", # 数据量: 24, token数: 930
+        "Middle_School_Biology": "初中生物", # 数据量: 26, token数: 964
+        "Middle_School_Mathematics": "初中数学", # 数据量: 24, token数: 1407
     },
+    # Social_Science数据量: 325, token数: 16771
     "Social_Science": {
-        "Teacher_Qualification": "教师资格", # 49
-        "Business_Administration": "工商管理", # 38
-        "Mao_Zedong_Thought": "毛泽东思想和中国特色社会主义理论体系概论", # 29
-        "Marxism": "马克思主义基本原理", # 24
-        "College_Economics": "大学经济学", # 60
-        "Education_Science": "教育学", # 34
-        "High_School_Geography": "高中地理", # 24
-        "High_School_Politics": "高中政治", # 24
-        "Middle_School_Geography": "初中地理", # 17
-        "Middle_School_Politics": "初中政治", # 26
+        "Teacher_Qualification": "教师资格", # 数据量: 49, token数: 2877
+        "Business_Administration": "工商管理", # 数据量: 38, token数: 1692
+        "Mao_Zedong_Thought": "毛泽东思想和中国特色社会主义理论体系概论", # 数据量: 29, token数: 958
+        "Marxism": "马克思主义基本原理", # 数据量: 24, token数: 542
+        "College_Economics": "大学经济学", # 数据量: 60, token数: 2418
+        "Education_Science": "教育学", # 数据量: 34, token数: 960
+        "High_School_Geography": "高中地理", # 数据量: 24, token数: 887
+        "High_School_Politics": "高中政治", # 数据量: 24, token数: 3731
+        "Middle_School_Geography": "初中地理", # 数据量: 17, token数: 598
+        "Middle_School_Politics": "初中政治", # 数据量: 26, token数: 2108
     },
+    # Humanity数据量: 312, token数: 17102
     "Humanity": {
-        "Professional_Tour_Guide": "导游资格", # 34
-        "Legal_Professional": "法律职业资格", # 28
-        "Art_Studies": "艺术学", # 38
-        "Chinese_Language_and_Literature": "中国语言文学", # 28
-        "Law": "法学", # 29
-        "Logic": "逻辑学", # 27
-        "Ideological_and_Moral_Cultivation": "思想道德修养与法律基础", # 24
-        "Modern_Chinese_History": "近代史纲要", # 28
-        "High_School_History": "高中历史", # 25
-        "High_School_Chinese": "高中语文", # 24
-        "Middle_School_History": "初中历史", # 27
+        "Professional_Tour_Guide": "导游资格", # 数据量: 34, token数: 1017
+        "Legal_Professional": "法律职业资格", # 数据量: 28, token数: 2260
+        "Art_Studies": "艺术学", # 数据量: 38, token数: 1025
+        "Chinese_Language_and_Literature": "中国语言文学", # 数据量: 28, token数: 666
+        "Law": "法学", # 数据量: 29, token数: 1894
+        "Logic": "逻辑学", # 数据量: 27, token数: 4233
+        "Ideological_and_Moral_Cultivation": "思想道德修养与法律基础", # 数据量: 24, token数: 524
+        "Modern_Chinese_History": "近代史纲要", # 数据量: 28, token数: 1117
+        "High_School_History": "高中历史", # 数据量: 25, token数: 1852
+        "High_School_Chinese": "高中语文", # 数据量: 24, token数: 1139
+        "Middle_School_History": "初中历史", # 数据量: 27, token数: 1375
     },
+    # Other数据量: 439, token数: 22889
     "Other": {
-        "Environmental_Impact_Assessment_Engineer": "环境影响评价工程师", # 36
-        "Urban_and_Rural_Planner": "注册城乡规划师", # 51
-        "Fire_Engineer": "注册消防工程师", # 36
-        "Physician": "医师资格", # 54
-        "Tax_Accountant": "税务师", # 54
-        "Accountant": "注册会计师", # 54
-        "Civil_Servant": "公务员", # 52
-        "Clinical_Medicine": "临床医学", # 27
-        "Basic_Medicine": "基础医学", # 24
-        "Plant_Protection": "植物保护", # 27
-        "Sports_Science": "体育学", # 24
+        "Environmental_Impact_Assessment_Engineer": "环境影响评价工程师", # 数据量: 36, token数: 2271
+        "Urban_and_Rural_Planner": "注册城乡规划师", # 数据量: 51, token数: 1727
+        "Fire_Engineer": "注册消防工程师", # 数据量: 36, token数: 1917
+        "Physician": "医师资格", # 数据量: 54, token数: 1552
+        "Tax_Accountant": "税务师", # 数据量: 54, token数: 2880
+        "Accountant": "注册会计师", # 数据量: 54, token数: 3426
+        "Civil_Servant": "公务员", # 数据量: 52, token数: 6052
+        "Clinical_Medicine": "临床医学", # 数据量: 27, token数: 1102
+        "Basic_Medicine": "基础医学", # 数据量: 24, token数: 485
+        "Plant_Protection": "植物保护", # 数据量: 27, token数: 843
+        "Sports_Science": "体育学", # 数据量: 24, token数: 634
     },
 }
 
